0ed821b2de4cf1b53dad4dc94fe94d64c56d5066
Morbius <Morghandenham097@riversideunified.org>
2024-05-10 14:16:01 -0700

    abababa

---
+++ s-2-final/Assets/scripts/session manage.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class sessionmanage : MonoBehaviour
+{
+    public int LevelOne; 
+    public int LevelTwo;
+    public int select;
+
+
+    public void onPlayButton()
+    {
+        SceneManager.LoadScene();
+    }
+    public void onQuitButton()
+    {
+
+    }
+
+    public void onLevelOne()
+    {
+
+    }
+    public void onLevelTwo( )
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
